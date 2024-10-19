1a4bb0de944d643e1a3fdefb9ff429fcf8082ef5
jalaj8009 <151819140+jalaj8009@users.noreply.github.com>
2024-10-19 17:35:53 +0530

    Update settings.py

--- SOS/SOS/settings.py
+++ SOS/SOS/settings.py
@@ -81,7 +81,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', 'django_simple'),
         'USER': os.getenv('DB_USER', 'root'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'root'),
-        'HOST': os.getenv('DB_HOST', 'db'),
+        # 'HOST': os.getenv('DB_HOST', 'db'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
         'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
